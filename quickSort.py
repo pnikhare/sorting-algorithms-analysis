c2ec82169b75bfe0b13d6fc898848793761fa02c
pnikhare <46647077+pnikhare@users.noreply.github.com>
2019-09-26 10:26:29 -0400

    Add files via upload

---
+++ quickSort.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+## QUICK SORT
+
+# Input : Array of Integers - arr
+# Output : Returns sorted array
+
+
+def quickSort(arr,low,high) :
+	if(low<high) :
+		pivotIndex = _partition(arr,low,high)
+		quickSort(arr,low,pivotIndex-1)
+		quickSort(arr,pivotIndex+1,high)
+
+	return arr
+
+
+def _partition(arr,low,high) :
+	## Pick first element as pivot element
+	pivot = arr[low]
+	#print(pivot,"low:",low,"high",high)
+	i=low+1
+	j=high
+	
+	while i<j :
+
+		while i<=high and arr[i]<pivot :
+			i=i+1
+		while j>low and arr[j]>=pivot:
+			j=j-1
+
+		if i<j :
+			swap(arr,i,j) 
+	if arr[low]>arr[j] :
+		swap(arr,low,j)
+	#print(arr)
+	return j
+
+#Generic Function to swap two elements at index 1,2
+def swap(arr,index1,index2) :
+	temp = arr[index1]
+	arr[index1] = arr[index2]
+	arr[index2] = temp
