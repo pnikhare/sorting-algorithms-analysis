8bdc8f7a223b14ce71cd5f2f71fc9ebcfb56a2df
Rahul Sundaresan <s.rahul@protonmail.com>
2019-09-28 17:23:56 -0400

    made dataset generator generate sorted and reverse sorted files as well. fixed a bug where generated dataset didn't have newlines

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,13 +1,31 @@
 import random
-import csv
 
 dataset_count = input("Datasets to generate:")
-dataset_range = input("How many numbers in each dataset?:")
+dataset_range = int(input("How many numbers in each dataset?:"))
 
+dataset_list = []
 for dataset_id in range(1, int(dataset_count) + 1):
-    dataset_name = "dataset_" + str(dataset_id) + "_" + str(dataset_count) + "_unsorted.csv"
-    with open(dataset_name, mode='w') as dataset_file:
-        dataset_writer = csv.writer(dataset_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for i in range(int(dataset_range)):
-            dataset_writer.writerow([random.randint(1, 100000)])
+    dataset_directory = "DataSet/"
+    dataset_unsorted_name = dataset_directory + "dataSet" + str(dataset_id) + ".txt"
+    dataset_sorted_name = dataset_directory + "dataSet" + str(dataset_id) + "_sorted.txt"
+    dataset_reverse_sorted_name = dataset_directory + "dataSet" + str(dataset_id) + "_unsorted.txt"
+
+    # generating and writing unsorted dataset
+    with open(dataset_unsorted_name, mode='a') as dataset_file:
+        for i in range(dataset_range):
+            random_number = random.randint(1, 100000)
+            dataset_file.write("%s\n" % random_number)
+            dataset_list.append(random_number)
+
+    #  generating and writing sorted dataset
+    dataset_list.sort()
+    with open(dataset_sorted_name, 'w') as dataset_sorted_file:
+        for number in dataset_list:
+            dataset_sorted_file.write("%s\n" % number)
+
+    #  generating and writing reverse sorted dataset
+    dataset_list.sort(reverse=True)
+    with open(dataset_reverse_sorted_name, 'w') as dataset_reverse_sorted_file:
+        for number in dataset_list:
+            dataset_reverse_sorted_file.write("%s\n" % number)
 
